acd5328ad099c6b2f42ba0ebd64a9b3f318ce7de
jj4170 <106699150+jj4170@users.noreply.github.com>
2023-02-10 19:03:21 +0530

    Changes-UI-editor-page
    Changes UI in editor page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,14 @@
 import React from "react";
 
-import resumeSvg from "../../assets/resume.svg";
+import resumeSvg from "../../assets/logo.svg";
 
 import styles from "./Header.module.css";
 
 function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
-        <p className={styles.heading}>
-          A <span>Resume</span> that stands out!
-        </p>
-        <p className={styles.heading}>
-          Make your own resume. <span>It's free</span>
-        </p>
-      </div>
-      <div className={styles.right}>
-        <img src={resumeSvg} alt="Resume" />
-      </div>
+      <img src={resumeSvg} alt="Logo" className={styles.logo} />
+      <span>TheAiHelper.com</span>
     </div>
   );
 }
